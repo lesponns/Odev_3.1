b016c78a56a725ecdfc52400ff79a9e0d5a8fd5b
lesponns <adamadakadam1903@gmail.com>
2023-08-06 20:38:30 +0300

    Add project files.

---
+++ Odev_3.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Kullanıcıdan
+            1 adet sayı talep edin, kullanıcı 5 in katı bir sayı girmezse program
+            kullanıcıdan tekrar tekrar sayı istesin.*/
+
+            int input = 0;
+            do
+            { 
+                Console.Write("Sayı değeri: ");
+                input = int.Parse(Console.ReadLine());
+            } 
+            while (input % 5 != 0);
+
+            Console.WriteLine("Girdiğiniz sayı 5'in katıdır. Sayı: " + input);
+            Console.ReadKey();
+
+        }
+    }
+}
